c4941efd42925bd7e974e1879c4f6678695adc15
sergivi11 <sergivi04@gmail.com>
2022-09-26 17:02:08 +0200

    PrimariFactors 2.0

--- src/main/java/org/example/PrimareFactors.java
+++ src/main/java/org/example/PrimareFactors.java
@@ -1,6 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimareFactors {
 
+    public List<Integer> descompose(int number){
 
+        List<Integer> primes = new ArrayList<>();
+
+        for (int candidate = 2; number > 1; ++candidate) {
+            for (; number % candidate == 0; number /= candidate) {
+                primes.add(candidate);
+            }
+        }
+        return primes;
+    }
 }
--- src/test/java/org/example/PrimareFactorsTest.java
+++ src/test/java/org/example/PrimareFactorsTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimareFactorsTest {
@@ -10,6 +12,32 @@ class PrimareFactorsTest {
     void TestingIfClassExsist(){
         new PrimareFactors();
     }
+
+    @Test
+    void givedOneWhenProcesedReturnEmptyArray(){
+        PrimareFactors solution = new PrimareFactors();
+        int numberOne = 1;
+        List<Integer> primareFactors = solution.descompose(numberOne);
+        assertEquals(0, primareFactors.size());
+    }
+
+    @Test
+    void givedTwoWhenProcesedReturnOneElementArry(){
+        PrimareFactors solution = new PrimareFactors();
+        int number = 2;
+        List<Integer> primareFactors = solution.descompose(number);
+        assertEquals(1, primareFactors.size());
+    }
+
+    @Test
+    void givedNineWhenProcesedReturnTwoElementArray(){
+        PrimareFactors solution = new PrimareFactors();
+        int number = 9;
+        List<Integer> primareFactors = solution.descompose(number);
+        assertEquals(2, primareFactors.size());
+    }
+
+
 
 
 
