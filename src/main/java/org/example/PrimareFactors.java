544129670c05a5bf7140c4f76e9660cf89ae29ba
sergivi11 <sergivi04@gmail.com>
2022-09-27 11:50:54 +0200

    PrimariFactors 3.0(ParametizedTest)

--- src/main/java/org/example/PrimareFactors.java
+++ src/main/java/org/example/PrimareFactors.java
@@ -4,16 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimareFactors {
-
     public List<Integer> descompose(int number){
-
         List<Integer> primes = new ArrayList<>();
-
-        for (int candidate = 2; number > 1; ++candidate) {
-            for (; number % candidate == 0; number /= candidate) {
-                primes.add(candidate);
+        for (int divisor = 2; number > 1; ++divisor) {
+            for (; number % divisor == 0; number /= divisor) {
+                primes.add(divisor);
             }
         }
         return primes;
     }
+
 }
--- src/test/java/org/example/PrimareFactorsTest.java
+++ src/test/java/org/example/PrimareFactorsTest.java
@@ -1,12 +1,19 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimareFactorsTest {
+    PrimareFactors solution = new PrimareFactors();
 
     @Test
     void TestingIfClassExsist(){
@@ -30,15 +37,31 @@ class PrimareFactorsTest {
     }
 
     @Test
-    void givedNineWhenProcesedReturnTwoElementArray(){
+    void gived18WhenProcesedReturnThreeElementArray(){
         PrimareFactors solution = new PrimareFactors();
-        int number = 9;
+        int number = 18;
         List<Integer> primareFactors = solution.descompose(number);
-        assertEquals(2, primareFactors.size());
+        assertEquals(3, primareFactors.size());
     }
 
+    @ParameterizedTest
+    @MethodSource("providedIntsAndTheExpectedResult")
+    void shouldPassNonNullMessageAsMethodParameter(int number, List<Integer> expected) {
+        List<Integer> primareFactors = solution.descompose(number);
+        assertEquals(expected, primareFactors);
+    }
 
-
+    private static Stream<Arguments> providedIntsAndTheExpectedResult() {
+        return Stream.of(
+                    Arguments.of(1, List.of()),
+                Arguments.of(2, List.of(2)),
+                Arguments.of(3, List.of(3)),
+                Arguments.of(4, List.of(2,2)),
+                Arguments.of(6, List.of(2,3)),
+                Arguments.of(8, List.of(2,2,2)),
+                Arguments.of(9, List.of(3,3))
+        );
+    }
 
 
 }
